0c604771263a43bf3f36afe9d6816fcd07dcf8d4
hyeyeon <guhaa03@daum.net>
2025-03-06 11:24:41 +0900

    Application.java에 Cosrs 설정코드 추가

--- src/main/java/com/exam/Application.java
+++ src/main/java/com/exam/Application.java
@@ -1,15 +1,31 @@
 package com.exam;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@Slf4j
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		System.out.println("Hello World!");
-		System.out.println("Hello World!");
+
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		log.info("WebMvcConfigurer.addCorsMappings");
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("*")
+						.allowedOrigins("http://localhost:3000","*");
+			}
+		};
 	}
 
 }
