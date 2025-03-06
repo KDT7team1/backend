d820c04983abc7786a52e9307b08479f39b7e480
minjeong <01minjung@gmail.com>
2025-03-06 17:29:11 +0900

    상품 등록 구현

--- src/main/java/com/exam/Application.java
+++ src/main/java/com/exam/Application.java
@@ -2,15 +2,25 @@ package com.exam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(Application.class, args);
-		System.out.println("awwwwwwwa");
 	}
 
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("*")
+						.allowedOrigins("http://localhost:3000", "*");
+			}
+		};
+	}
 }
