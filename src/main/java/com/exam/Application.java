ada4c7787deb5d16d43d4673946a406f949de7bb
Hyemin <star970909@gmail.com>
2025-03-21 15:49:59 +0900

    Application.java에 @Override 추가 - 실행 안되는 오류 수정

--- src/main/java/com/exam/Application.java
+++ src/main/java/com/exam/Application.java
@@ -19,6 +19,7 @@ public class Application {
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
+			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
 						.allowedMethods("*")
