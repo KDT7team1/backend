79f96103741b4a2dc822942e45374ec4913725a3
Hyemin <star970909@gmail.com>
2025-03-27 21:35:13 +0900

    매출 알람 기능 구현 #9 리퀘스트 매핑 - URL 변경 - 요청 시 발생되는 오류 수정

--- src/main/java/com/exam/salesAnalysis/SalesAnalysisService.java
+++ src/main/java/com/exam/salesAnalysis/SalesAnalysisService.java
@@ -12,11 +12,11 @@ public interface SalesAnalysisService {
     // 특정 날짜와 시간대의 매출 데이터 조회
     List<SalesDailyDTO> getSalesDailyByDateAndHour(LocalDate salesDate, int salesHour);
 
-    // 지난 7일간의 시간대 평균 매출액/주문량과 매출 데이터
-    Map<String, Object> getAverageSalesLast7Days(LocalDate salesDate, int salesHour);
+    // 지난 7일간의 시간대 평균 매출액
+    long getAverageSalesLast7Days(LocalDate salesDate, int salesHour);
 
-    // 지난 30일간의 시간대 평균 매출액/주문량과 매출 데이터
-    Map<String, Object> getAverageSalesLast30Days(LocalDate salesDate, int salesHour);
+    // 지난 30일간의 시간대 평균 매출액
+    long getAverageSalesLast30Days(LocalDate salesDate, int salesHour);
 
     // 오늘 매출 데이터와 비교하여 이상 감지
     void detectSalesAnomaly(LocalDate salesDate, int salesHour);
