97c962701f054063128d7f67dad97eca60b170c2
Hyemin <star970909@gmail.com>
2025-04-02 13:04:06 +0900

    상품 조회 필터링 구현

--- src/main/java/com/exam/shop/ShopServiceImpl.java
+++ src/main/java/com/exam/shop/ShopServiceImpl.java
@@ -7,8 +7,10 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
@@ -21,9 +23,9 @@ public class ShopServiceImpl implements ShopService {
     }
 
     @Override
-    public Page<GoodsDTO> getGoods(Long category, String search, Pageable pageable) {
+    public Page<GoodsDTO> getGoods(Long category, String search, Long minPrice, Long maxPrice, Pageable pageable) {
         // 필터링 & 검색 추가
-        Page<Goods> goodsPage = shopRepository.findAllByFilters(category, search, pageable);
+        Page<Goods> goodsPage = shopRepository.findAllByFilters(category, search, minPrice, maxPrice, pageable);
 
         // Goods → GoodsDTO 변환
         List<GoodsDTO> goodsDTOList = goodsPage.getContent().stream().map(item ->
@@ -95,5 +97,40 @@ public class ShopServiceImpl implements ShopService {
                     return dto;
                 }).collect(Collectors.toList());
         return goodsDTOList;
+    }
+
+    @Override
+    public List<Map<String, Object>> findTop10GoodsForAWeek(LocalDate targetDate) {
+
+        LocalDateTime startDate = targetDate.minusDays(8).atTime(0, 0, 0);
+        LocalDateTime endDate = targetDate.minusDays(1).atTime(23, 59, 59);
+
+        List<Object[]> entityList = shopRepository.findTop10GoodsForAWeek(startDate, endDate);
+
+        return entityList.stream()
+                .map(row -> {
+                    Goods goods = (Goods) row[0]; // Goods 엔티티
+                    Long weeklySales = (Long) row[1]; // 판매량
+
+                    return Map.of(
+                            "goodsDTO", GoodsDTO.builder()
+                                                .goods_id(goods.getGoods_id())
+                                                .category_id(goods.getCategory().getCategory_id())
+                                                .goods_name(goods.getGoods_name())
+                                                .goods_price(goods.getGoods_price())
+                                                .goods_description(goods.getGoods_description())
+                                                .goods_image(goods.getGoods_image())
+                                                .goods_stock(goods.getGoods_stock())
+                                                .goods_created_at(goods.getGoods_created_at())
+                                                .goods_updated_at(goods.getGoods_updated_at())
+                                                .goods_orders(goods.getGoods_orders())
+                                                .originalPrice(goods.getOriginalPrice())
+                                                .discountRate(goods.getDiscountRate())
+                                                .discountEndAt(goods.getDiscountEndAt())
+                                                .build(),// Goods → GoodsDTO 변환
+                            "weeklySales", weeklySales // 일주일 판매량
+                    );
+                })
+                .toList();
     }
 }
